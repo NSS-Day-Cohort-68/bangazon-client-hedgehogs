39ba1a3f23a128faeed67f2a02e8518c62b0bf8c
Saygin Gecener <sgecener@yahoo.com>
2024-04-12 12:19:42 -0400

    log removed

--- pages/profile.js
+++ pages/profile.js
@@ -12,7 +12,6 @@ export default function Profile() {
 
   useEffect(() => {
     getUserProfile().then((profileData) => {
-      console.log(profileData);
       if (profileData) {
         setProfile(profileData);
       }
