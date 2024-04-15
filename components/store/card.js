3a1f21723a8ab472bc7dd06327508f3515b2200a
Jean Patimeteeporn <jpatimet@gmail.com>
2024-04-15 15:11:02 -0500

    displayed a 4 product list on  each store card

--- components/store/card.js
+++ components/store/card.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { ProductCard } from "../../components/product/card";
 
 export function StoreCard({ store, width = "is-half" }) {
   return (
@@ -11,6 +12,11 @@ export function StoreCard({ store, width = "is-half" }) {
           <p className="content">Owner: {store.seller.user.username}</p>
           <div className="content">{store.description}</div>
           <div className="content"> {store.products.length} items for sale</div>
+        </div>
+        <div className="columns is-multiline">
+          {store.products.slice(0, 4).map((product) => (
+            <ProductCard product={product} key={product.id} />
+          ))}
         </div>
         <footer className="card-footer">
           <Link href={`stores/${store.id}`}>
