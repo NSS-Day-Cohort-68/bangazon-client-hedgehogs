d67cef12a8032d67b605d80a68155e28dfaccf13
Dale Dear <kristendaledear@gmail.com>
2024-04-17 11:29:58 -0500

    commit for fixed merge conflict

--- components/order/detail.js
+++ components/order/detail.js
@@ -19,11 +19,7 @@ export default function CartDetail({ cart, removeProduct }) {
               <td>{lineitem.product.name}</td>
               <td>{lineitem.product.price}</td>
               <td>
-<<<<<<< HEAD
-                <span className="icon is-clickable" onClick={() => removeProduct(lineitem.id)}>
-=======
                 <span className="icon is-clickable" onClick={() => removeProduct(lineitem.product.id)}>
->>>>>>> 631d212ef80886a0e9b76649d5886249d2685229
                   <i className="fas fa-trash"></i>
                 </span>
               </td>
